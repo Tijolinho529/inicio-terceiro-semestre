4337669a79430fa1d90e78c997681d0b1758b29d
Tijolinho529 <161033643+Tijolinho529@users.noreply.github.com>
2025-02-06 00:03:06 +0000

    inicio-terceiro-semestre

--- Atividade1.java
+++ Atividade1.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class Atividade1 {
 
-    public static
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numero1;
+        int numero2;
+        int soma;
+        int subtacao;
+        int multiplicacao;
+        double divisao;
+        
+            System.out.println("Bota um número ae: ");
+            numero1 = n.nextInt ();
+            System.out.println("Bota outro: ");
+            numero2 = n.nextInt ();
+            soma = numero1 + numero2;
+            
+            
+        
+    }
     
 }
