14ae3d66143e5973c9ce617c92d478a7174d408b
Tijolinho529 <161033643+Tijolinho529@users.noreply.github.com>
2025-02-07 00:17:56 +0000

    inicio-terceiro-semestre

--- Atividade4.java
+++ Atividade4.java
@@ -8,6 +8,10 @@ public class Atividade4 {
         System.out.println("Namoral, escreve algo ae, se escrever número ou símbolo é gay: ");
         String palavra1 = scanner.next();
         System.out.println("Bota outra seu queima-rosca: ");
+        String palavra2 = scanner.next();
+
+        String palavraConcatenada = palavra1 + palavra2;
+        System.out.println("Palavra Concatenada: " + palavraConcatenada);
 
         
     }
